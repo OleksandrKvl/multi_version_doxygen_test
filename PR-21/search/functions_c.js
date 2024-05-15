27f574bc5379efe7003a05ca9d280facdf83fd1b
OleksandrKvl <OleksandrKvl@users.noreply.github.com>
2024-05-15 19:46:28 +0000

    deploy: ee6952878ec44de526d837525e5edf7838653303

--- PR-21/search/functions_c.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['stringdebug_0',['stringDebug',['../classString.html#a5c07384b505d25ae6f61fc7abf0b0e61',1,'String']]]
-];
--- PR-21/search/pages_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['notes_0',['NOTES',['../md__2home_2runner_2work_2multi__version__doxygen__test_2multi__version__doxygen__test_2NOTES.html',1,'']]]
-];
